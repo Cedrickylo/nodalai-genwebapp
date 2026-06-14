514d96afeea99dcaa2de59972f225f5d73cb0a6d
Cedrickylo <jc.siason08@gmail.com>
2026-06-14 08:45:52 +0800

    Fix: database connection

--- netlify/functions/utils/db.js
+++ netlify/functions/utils/db.js
@@ -2,7 +2,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.NETLIFY_DB_URL,
+  connectionString: process.env.netlifydb_owner,
   ssl: {
     rejectUnauthorized: false
   }
